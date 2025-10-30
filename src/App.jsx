a5626a0477853725ab5ad4d453d20530e4477caa
JaeYun <sim_518@naver.com>
2025-10-30 16:41:21 +0900

    훑어보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css'
 
-function App() {
-
+function MyButton() {
+  /* React 앱은 컴포넌트로 구성됩니다.
+  컴포넌트는 고유한 로직과 모양을 가진 사용자 인터페이스 UI의 일부입니다. */
   return (
-    
+    <button>버튼</button>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h1>리액트에 온걸 환영한다</h1>
+      <MyButton /> {/*컴포넌트는 항상 대문자로 시작*/}
+    </div>
   );
 }
 
