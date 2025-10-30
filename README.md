189c131a6e5b5f0fdb96d6aaf48a1690f8aff2ac
JaeYun <sim_518@naver.com>
2025-10-30 16:48:32 +0900

    컴포넌트 생성 및 중첩하기 & JSX로 마크업 작성하기 & 스타일 추가하기

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-study React
+## study React
 https://ko.react.dev/learn
+
+- 빠르게 시작하기
--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,26 @@ function MyButton() {
   )
 }
 
+function AboutPage() {
+  return (
+    <> {/*이렇게*/}
+      <h1>여러 개의 태그를 반환할 수 없음</h1>
+      <p>div 또는 빈 태그로 감싸야함</p>
+      <p>br 같은 애들도 <br /> 항상 닫아야함</p>
+    </>
+  )
+}
+
+
 function App() {
   return (
     <div>
       <h1>리액트에 온걸 환영한다</h1>
       <MyButton /> {/*컴포넌트는 항상 대문자로 시작*/}
+      <hr />
+      <AboutPage />
+      <hr />
+      <img className='avatar' /> {/*HTML에 class가 여기서는 className*/}
     </div>
   );
 }
